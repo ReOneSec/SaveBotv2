630198ed6e02d9d317f1173701d5c34bac8cd4f9
ReOneSec <reonesecapis@gmail.com>
2025-07-19 08:19:51 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -39,14 +39,14 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("You are Banned. Contact -- @devgaganin")
+            await message.reply_text("You are Banned. Contact -- @aViperROX")
             return 1
       except UserNotParticipant:
         caption = f"Join our channel to use the bot"
         await message.reply_photo(photo="https://graph.org/file/d44f024a08ded19452152.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
         return 1
       except Exception:
-         await message.reply_text("Something Went Wrong. Contact us @devgaganin...")
+         await message.reply_text("Something Went Wrong. Contact us @aViperROX ...")
          return 1
 async def get_seconds(time_string):
     def extract_value_and_unit(ts):
@@ -86,6 +86,7 @@ PROGRESS_BAR = """\n
 │ **__Bytes:__** {0}%
 │ **__Speed:__** {3}/s
 │ **__ETA:__** {4}
+| **__Powered by 🇩 🇮 🇬 🇬 🇮 🇩 🇮 🇬 🇮__**
 ╰─────────────────────╯
 """
 async def progress_bar(current, total, ud_type, message, start):
@@ -250,7 +251,7 @@ async def progress_callback(current, total, progress_message):
     f"│ **__Progress:__** {percent:.2f}%\n"
     f"│ **__Uploaded:__** {current_mb:.2f} MB / {total_mb:.2f} MB\n"
     f"╰──────────────────╯\n\n"
-    f"**__Powered by Team SPY__**"
+    f"**__Powered by 🇩 🇮 🇬 🇬 🇮 🇩 🇮 🇬 🇮__**"
         )
 
         last_update_time = current_time
